f9946858a3956d3eb2cb7e2199780f44e28bc2a0
sasinn98 <sasinn98@gmail.com>
2017-04-23 19:15:49 +0700

    remodularized

--- KaideeDB.py
+++ KaideeDB.py
@@ -1,25 +1,13 @@
-
-from flask import Flask ,render_template
 from flask import jsonify
 from flask import request
-from flask_cors import CORS, cross_origin
 import mysql.connector
 from mysql.connector import errorcode
+import mysql_config
 
-app = Flask(__name__)
-CORS(app)
-
-
-MYSQL_SETTING = {
-  'user':'root',
-  'password':'root',
-  'database':'Kaidee',
-  'host':'localhost',
-}
-
+#app = Flask(__name__)
 
 def queryDB(query):
-  cnx = mysql.connector.connect(**MYSQL_SETTING)
+  cnx = mysql.connector.connect(**mysql_config.config)
   cursor = cnx.cursor(dictionary=True)
   cursor.execute(query)
   result = dict()
@@ -28,14 +16,13 @@ def queryDB(query):
   for row in cursor:
     result.update(row)
     url.append(row["url"])
-  print(url)
   result.update({"url":url})
   cursor.close()
   cnx.close()
   return result
 
 def insertDB(query):
-  cnx = mysql.connector.connect(**MYSQL_SETTING)
+  cnx = mysql.connector.connect(**mysql_config.config)
   cursor = cnx.cursor()
   cursor.execute(query)
   result = []
@@ -44,10 +31,11 @@ def insertDB(query):
   cnx.commit() 
   cursor.close()
   cnx.close()
-  return result  
+  return result
+
 # send userid return rid,buyer_uid,seller_uid,iid,sid,name for all room that used by that user 
 # example localhost:5000/ChatRooms/123
-@app.route('/ChatRooms/<uid>', methods=['GET'])
+#@app.route('/ChatRooms/<uid>', methods=['GET'])
 def getChatRooms(uid):
   query = ("SELECT * FROM ChatRooms NATURAL JOIN ServiceOffering NATURAL JOIN Services WHERE buyer_uid =\'" + uid + "\' OR seller_uid =\'" + uid + "\'")
   result = queryDB(query)
@@ -56,7 +44,7 @@ def getChatRooms(uid):
   
 # send rid return rid,timestamp,title,message,form_url,image_url for all message in that rid 
 # example localhost:5000/Messages/123
-@app.route('/Messages/<rid>', methods=['GET'])
+#@app.route('/Messages/<rid>', methods=['GET'])
 def get_Messages(rid):
   query = ("SELECT * FROM ChatMessages WHERE rid =\'" + rid + "\'")
   result = queryDB(query)
@@ -64,7 +52,7 @@ def get_Messages(rid):
 
 # will finish 
 # LOL
-@app.route('/Messages/', methods=['POST'])
+#@app.route('/Messages/', methods=['POST'])
 def post_Messages():
   query = ("INSERT INTO ChatMessages")
   result = insertDB(query)
@@ -72,7 +60,7 @@ def post_Messages():
 
 # send uid return uid,first_name,last_name,phone,email,profile_pic
 # example localhost:5000/Users/123
-@app.route('/Users/<uid>', methods=['GET'])
+#@app.route('/Users/<uid>', methods=['GET'])
 def get_Users(uid):
   query = ("SELECT * FROM Users WHERE uid =\'" + uid + "\'")
   result = queryDB(query)
@@ -80,7 +68,7 @@ def get_Users(uid):
 
 # will need to change to POST will make it by the end of the week
 # will fix later don't know how to autogen fid 
-@app.route('/Feedback/<buyer_uid>/<seller_uid>/<rating>/<comments>/<iid>', methods=['GET'])
+#@app.route('/Feedback/<buyer_uid>/<seller_uid>/<rating>/<comments>/<iid>', methods=['GET'])
 def post_Feedback(buyer_uid,seller_uid,rating,comments):
   query = ("INSERT INTO Feedback")
   result = insertDB(query)
@@ -89,33 +77,30 @@ def post_Feedback(buyer_uid,seller_uid,rating,comments):
 #need to get seller_uid and do another query for more seller information possible for frontend to call getUsers again
 #will return  iid, name, description, price, cid, since, seller_uid, product_location
 #might need another table to collect product location
-@app.route('/Product/<iid>', methods=['GET'])
+#@app.route('/Product/<iid>', methods=['GET'])
 def get_Product(iid):
   query = ("SELECT *, i.name AS pname, c.name AS cname FROM item_listing i NATURAL JOIN product_images_relation pir NATURAL JOIN product_images pi JOIN categories c ON i.cid=c.cid JOIN addresses a ON i.seller_uid=a.uid JOIN users u ON i.seller_uid=u.uid WHERE i.iid=" + iid)
   result = queryDB(query)
   return jsonify(result)
   
 #send iid return iid,img_id,url,name for all of images for that item
-@app.route('/Images/<iid>', methods=['GET'])
+#@app.route('/Images/<iid>', methods=['GET'])
 def get_Images(iid):
   query = ("SELECT * FROM ProductImages WHERE iid = \'"+ iid +"\'")
   result = queryDB(query)
   return jsonify({'result' : result})
 
 #need to discuss later
-@app.route('/ProductForCategory/<cid>', methods=['GET'])
+#@app.route('/ProductForCategory/<cid>', methods=['GET'])
 def get_product_category(cid):
   query = ("")
   result = queryDB(query)
   return jsonify({'result' : result})
 
 #need to discuss later
-@app.route('/RecommendedProducts/<uid>', methods=['GET'])
+#@app.route('/RecommendedProducts/<uid>', methods=['GET'])
 def get_Recommended(uid):
   query = ("")
   result = queryDB(query)
   return jsonify({'result' : result})
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
--- Server.py
+++ Server.py
@@ -1,21 +1,21 @@
-import sys
-
-sys.path.append('/ENV/Lib')
-
 from flask import Flask, current_app
-from flask import jsonify
-from KaideeDB import KaideeDB
+from flask_cors import CORS, cross_origin
+import KaideeDB as db
 
 app = Flask(__name__)
-db = KaideeDB()
+CORS(app)
 
 @app.route("/")
 def index():
-    return current_app.send_static_file('index.html')
+  return current_app.send_static_file('index.html')
+
+@app.route('/Product/<iid>', methods=['GET'])
+def get_Product(iid):
+  return db.get_Product(iid)
 
 @app.route("/sql")
 def select():
-	return jsonify(db.selectAll())
-	
+  return jsonify(db.selectAll())
+
 if __name__ == "__main__":
-    app.run()
+  app.run(debug=True)
